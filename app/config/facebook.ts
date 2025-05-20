653a04aacd0810ba103d7000041fd13776b48666
CDR <eucodrut69@gmail.com>
2025-05-20 20:18:13 +0300

    Initial commit (Next.js + Tailwind project scaffold)

---
+++ app/config/facebook.ts
@@ -0,0 +1,6 @@
+export const FACEBOOK_CONFIG = {
+  appId: '1050216867047079',
+  appSecret: 'd88919f7836aa23c5edb3c3c3310d19c',
+  apiVersion: 'v18.0',
+  baseUrl: 'https://graph.facebook.com',
+} 
